d6fbf7cb6980da7877c30f53dbf4fb9c2f2cc1f1
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2020-06-30 17:12:33 -0700

    Int_as_array_increment
    omg omg omg omg omg i passed the first time, I think it works a lot better if I write down the steps

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -4,8 +4,24 @@ from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
-    last_digit = len(A) - 1
-    
+    # OMG OMG OMG I PASSED THE FIRST TIME!
+    # OMG OMG OMG OMG OMG
+    digit_to_change = len(A) - 1
+    if A[digit_to_change] != 9:
+        A[digit_to_change] += 1
+        return A
+    else:
+        while A[digit_to_change] == 9:
+            A[digit_to_change] = 0
+            if digit_to_change == 0:
+                A.insert(0, 1)
+                return A
+            elif A[digit_to_change - 1] != 9:
+                A[digit_to_change - 1] += 1
+                return A
+            else:
+                digit_to_change -= 1
+            
 
 
 if __name__ == '__main__':
